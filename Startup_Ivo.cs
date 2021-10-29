89fad6f8a0f559b7ba8253513c89fb6db57cc43e
ivost9 <92783350+ivost9@users.noreply.github.com>
2021-10-29 10:23:27 +0300

    Add files via upload

---
+++ Startup_Ivo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace GitExercise
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            bool isAuthorized = CheckCredentials();
+
+            if (!isAuthorized)
+            {
+                Console.WriteLine("Access denied.");
+                Console.ReadKey(intercept: true);
+                return;
+            }
+            Console.WriteLine("Console Calculator App");
+            Console.WriteLine(new string('-', 15));
+
+            Console.Write("a = ");
+            double a = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+            Console.Write("b = ");
+            double b = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+            Console.WriteLine("Choose one from the listed options:");
+            foreach (string option in OptionsManager.OptionsList)
+            {
+                Console.WriteLine($"\t{option}");
+            }
+
+            Console.Write("Option: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "a":
+                    OptionsManager.Add(a, b);
+                    break;
+                case "s":
+                    OptionsManager.Subtract(a, b);
+                    break;
+                case "m":
+                    OptionsManager.Multiply(a, b);
+                    break;
+                case "d":
+                    OptionsManager.Divide(a, b);
+                    break;
+                case "sabs":
+                    OptionsManager.SubtractAbs(a, b);
+                    break;
+            }
+
+            Console.WriteLine("Press any key to close the app...");
+            Console.ReadKey(true);
+        }
+    }
+}
