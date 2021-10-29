fded4b0e62cc8afdeff9d1d01b48eada5580165a
Ivan0904 <92783533+Ivan0904@users.noreply.github.com>
2021-10-29 10:38:53 +0300

    Add files via upload

---
+++ GitExercise/Startup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace GitExercise
+{
+    public class Startup
+    {
+        private static bool CheckCredentails()
+        {
+            Console.Write("Enter password to gain access");
+            string password = Console.ReadLine();
+            Console.Clear();
+
+            return password == Password;
+        }
+        private const string Password = "abcd1234";
+        public static void Main()
+        {
+            bool isAuthorized = CheckCredentails();
+
+            if (!isAuthorized)
+            {
+                Console.WriteLine("Access denied.");
+                Console.ReadKey(intercept: true);
+                return;
+            }
+
+            Console.WriteLine("Console Calculator App");
+            Console.WriteLine(new string('-', 15));
+
+            Console.Write("a = ");
+            double a = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+            Console.Write("b = ");
+            double b = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+            Console.WriteLine("Choose one from the listed options:");
+            foreach (string option in OptionsManager.OptionsList)
+            {
+                Console.WriteLine($"\t{option}");
+            }
+
+            Console.Write("Option: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "a":
+                    OptionsManager.Add(a, b);
+                    break;
+                case "s":
+                    OptionsManager.Subtract(a, b);
+                    break;
+                case "m":
+                    OptionsManager.Multiply(a, b);
+                    break;
+                case "d":
+                    OptionsManager.Divider(a, b);
+                    break;
+                case "sabs":
+                    OptionsManager.SubtractAbs(a, b);
+                    break;
+            }
+
+            Console.WriteLine("Pres any key to close the app...");
+            Console.ReadKey(true);
+        }
+    }
+}
